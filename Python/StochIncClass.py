03356b9f56aa7b3a9a87057b0a48f07cad47ab2a
JTGardiner <justintgardiner@gmail.com>
2015-09-22 12:18:51 -0600

    Added some starter code

--- Python/StochIncClass.py
+++ Python/StochIncClass.py
@@ -1,9 +1,9 @@
 import time
- 
+
 import numpy as np
 from scipy.optimize import fsolve
- 
-Class OG(object):
+
+class OG(object):
     """
     
     """
@@ -21,15 +21,15 @@ Class OG(object):
         nvec[2*S/3:] = nvec[2*S/3:]*.3
         self.nvec = nvec
         self.lambda_bar = self.get_lambda_bar(Pi)
- 
+
         (self.A,
          self.alpha,
          self.delta_annual) = firm_params
         self.delta = 1-(1-delta_annual)**(80/self.S)
     
-    def get_lambda_bar(Pi):
+    def get_lambda_bar(self, Pi):
         """Compute the ergodic distribution of the Markox chain."""
-        w, vr = la.eig(Pi)
+        w, vr = np.linalg.eig(Pi)
         lambda_bar = vr[:,np.isclose(w,1.0)]
         lambda_bar /= sum(lambda_bar)
         lambda_bar = lambda_bar.flatten()
@@ -37,7 +37,7 @@ Class OG(object):
     
     def initialize_state(self):
         """Initialize a random starting state."""
-        S0 = np.random.multinomial(200, np.ones(20)/20.)
+        S0 = np.random.multinomial(self.N, np.ones(self.S)/float(self.S))
         e0 = np.array([np.random.multinomial(s, lambda_bar) for s in S0])
         b0 = [list(np.random.rand(e)) for e in e0[1:].ravel()]
         b0 = [[0],[0]]+b0
@@ -75,16 +75,16 @@ Class OG(object):
             
 # Define the Household parameters
 N = 100000
-S = 3
+S = 80
 J = 2
 beta_annual = .96
 sigma = 3.0
 Pi = np.array([[0.6, 0.4],
                [0.4, 0.6]])
- 
+
 # lambda_bar = get_lambda_bar(Pi)
 e_jt = np.array([0.8, 1.2])
- 
+
 # S = 4
 # J = 7
 # Pi = np.array([[0.40,0.30,0.24,0.18,0.14,0.10,0.06],
@@ -95,24 +95,21 @@ e_jt = np.array([0.8, 1.2])
 #                [0.10,0.14,0.18,0.24,0.30,0.40,0.30],
 #                [0.06,0.10,0.14,0.18,0.24,0.30,0.40]])
 # Pi = (Pi.T/Pi.sum(1)).T
- 
+
 # lambda_bar = get_lambda_bar(Pi)
 # e_jt = np.array([0.6, 0.8, 1.0, 1.3, 1.6, 2.0, 2.5])
- 
+
 household_params = (N, S, J, beta_annual, sigma, Pi, e_jt)
- 
+
 # Firm parameters
 A = 1.0
 alpha = .35
 delta_annual = .05
- 
-firm_parameter = (A, alpha, delta_annual)
- 
+
+firm_params = (A, alpha, delta_annual)
+
 # SS parameters
 b_guess = np.ones((S, J))*.0001
 b_guess[0] = np.zeros(J)
 SS_tol = 1e-10
 rho = .5
-In [ ]:
-
- 
