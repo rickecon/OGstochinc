fdc06855a2a314f2bc7ab7acb4ae96c959243963
Rex McArthur <drexmcarthur@gmail.com>
2015-10-27 19:19:26 -0600

    update to master

--- Python/StochIncClass.py
+++ Python/StochIncClass.py
@@ -4,9 +4,6 @@ import numpy as np
 from scipy.optimize import fsolve
 
 class OG(object):
-    """
-    
-    """
     def __init__(self, household_params, firm_params):
         """Instatiate the state parameters of the OG model."""
         (self.N,
@@ -15,95 +12,149 @@ class OG(object):
          self.beta_annual,
          self.sigma,
          self.Pi,
-         self.e_jt) = household_params
-        self.beta = beta_annual**(80/self.S)
+         self.e_jt,
+         self.mean,
+         self.std) = household_params
+        self.beta = self.beta_annual**(80/self.S)
 
-        #Set labor exogenously
         nvec = np.ones(S)
         nvec[2*S/3:] = nvec[2*S/3:]*.3
         self.nvec = nvec
-
-        self.lambda_bar = self.get_lambda_bar()
+        self.lambda_bar = self.get_lambda_bar(self.Pi)
 
         (self.A,
          self.alpha,
          self.delta_annual) = firm_params
-
-        self.delta = 1-(1-delta_annual)**(80/self.S)
-
+        self.delta = 1-(1-self.delta_annual)**(80/self.S)
     
 
     def set_state(self):
-        '''
-        Here is some kind of function that sets all values, after
-        parameters are all set
-        '''
+        """Set initial state and r and w."""
         self.initialize_b_vec()
         self.get_r_and_w()
         #self.get_c()
 
 
-    def get_lambda_bar(self):
+    def get_lambda_bar(self, Pi):
         """Compute the ergodic distribution of the Markox chain."""
-        w, vr = np.linalg.eig(Pi)
+        w, vr = np.linalg.eig(Pi.T)
         lambda_bar = vr[:,np.isclose(w,1.0)]
         lambda_bar /= sum(lambda_bar)
         lambda_bar = lambda_bar.flatten()
         return lambda_bar
     
+
     def initialize_b_vec(self):
         """Initialize a random starting state."""
-        e0 = np.ones((self.S,self.J))*float(self.N)/(self.S*self.J)
-        b0 = [list(np.random.rand(e)) for e in e0[1:].ravel()]
-        b0 = [[0],[0]]+b0
-        b0 = np.array(b0)
-        self.b = b0
-
-    def get_r_and_w(self, percent=False):
+        N = int(np.sum(100*S*lambda_bar))
+        [np.exp(self.std*np.random.randn(S,round(b)))+self.mean
+         for b in self.lambda_bar*self.N/self.S]
+        
+        
+    def get_r_and_w(self):
         """Calculate r and w at the current state."""
-        if percent:
-            self.K = sum(self.b)
-        else:
-            self.K = np.array(self.b.sum()).sum()
-        self.L = sum(self.nvec)
-        self.r = self.alpha*self.A*(self.L/self.K)**(1-self.alpha)-self.delta
-        self.w = (1-self.alpha)*self.A*(self.K/self.L)**self.alpha
-        
-    def get_euler_errors(self, b):
-        """Compute the euler errors."""
-        
-        
-    def update_state(self):
-        """Solve the individual lifetime problems."""
-        self.b = fsolve(get_euler_errors)
-        
-    def SS(self, SS_params):
-        """Find the steady state."""
-        tol = SS_params
-        diff = 1
-        while diff<tol:
-            current_state = self.state
-            self.update_state()
-            diff = np.linalg.norm(current_state-self.state)
+        self.K = sum(list(self.state.sum()))
+        self.L = (self.nvec*self.S).sum()
+        self.r = self.alpha*self.A*(L/K)**(1-self.alpha)-self.delta
+        self.w = (1-self.alpha)*self.A*(K/L)**self.alpha
 
-    def _calc_u(self):
-        """
-        Calculates the utility, given consumption
-        """
-        utility = (self.c**(1-self.sigma)-1)/(1-self.sigma)
-        return utility
-
-    def get_c(self):
-        '''
-        Returns S length Consumption vector
-        '''
-        r, w = self.r, self.w
-        b = np.copy(self.b)
-
-        b_s = np.vstack(np.zeros((1,J), b))
-        b_sp1 = np.append(b, np.zeros((1,J)))
-        self.c = ((1 + r) * b_s + w * nvec - b_sp1)
-        self.c_cstr = cvec <= 0
+#    def __init__(self, household_params, firm_params):
+#        """Instatiate the state parameters of the OG model."""
+#        (self.N,
+#         self.S,
+#         self.J,
+#         self.beta_annual,
+#         self.sigma,
+#         self.Pi,
+#         self.e_jt) = household_params
+#        self.beta = beta_annual**(80/self.S)
+#        self.Pi = self.Pi.T
+#
+#        #Set labor exogenously
+#        nvec = np.ones(S)
+#        nvec[2*S/3:] = nvec[2*S/3:]*.3
+#        self.nvec = nvec
+#        self.lambda_bar = self.get_lambda_bar()
+#
+#        (self.A,
+#         self.alpha,
+#         self.delta_annual) = firm_params
+#
+#        self.delta = 1-(1-delta_annual)**(80/self.S)
+#
+#        self.set_state()
+#
+#    
+#
+#    def set_state(self):
+#        '''
+#        Here is some kind of function that sets all values, after
+#        parameters are all set
+#        '''
+#        self.initialize_b_vec()
+#        self.get_r_and_w()
+#        #self.get_c()
+#
+#
+#    def get_lambda_bar(self):
+#        """Compute the ergodic distribution of the Markox chain."""
+#        w, vr = np.linalg.eig(self.Pi)
+#        lambda_bar = vr[:,np.isclose(w,1.0)]
+#        lambda_bar /= sum(lambda_bar)
+#        lambda_bar = lambda_bar.flatten()
+#        return lambda_bar
+#    
+#    def initialize_b_vec(self):
+#        """Initialize a random starting state."""
+#        e0 = np.ones((self.S,self.J))*float(self.N)/(self.S*self.J)
+#        print e0
+#
+#
+#    def get_r_and_w(self, percent=False):
+#        """Calculate r and w at the current state."""
+#        if percent:
+#            self.K = sum(self.b)
+#        else:
+#            self.K = np.array(self.b.sum()).sum()
+#        self.L = sum(self.nvec)
+#        self.r = self.alpha*self.A*(self.L/self.K)**(1-self.alpha)-self.delta
+#        self.w = (1-self.alpha)*self.A*(self.K/self.L)**self.alpha
+#        
+#    def get_euler_errors(self, b):
+#        """Compute the euler errors."""
+#        
+#        
+#    def update_state(self):
+#        """Solve the individual lifetime problems."""
+#        self.b = fsolve(get_euler_errors)
+#        
+#    def SS(self, SS_params):
+#        """Find the steady state."""
+#        tol = SS_params
+#        diff = 1
+#        while diff<tol:
+#            current_state = self.state
+#            self.update_state()
+#            diff = np.linalg.norm(current_state-self.state)
+#
+#    def _calc_u(self):
+#        """
+#        Calculates the utility, given consumption
+#        """
+#        utility = (self.c**(1-self.sigma)-1)/(1-self.sigma)
+#        return utility
+#
+#    def get_c(self):
+#        '''
+#        Returns S length Consumption vector
+#        '''
+#        r, w = self.r, self.w
+#        b = np.copy(self.b)
+#
+#        b_s = np.vstack(np.zeros((1,J), b))
+#        b_sp1 = np.append(b, np.zeros((1,J)))
+#        self.c = ((1 + r) * b_s + w * nvec - b_sp1)
+#        self.c_cstr = cvec <= 0
 
 # Define the Household parameters
 N = 800
@@ -111,8 +162,10 @@ S = 80
 J = 2
 beta_annual = .96
 sigma = 3.0
-Pi = np.array([[0.6, 0.4],
-               [0.4, 0.6]])
+Pi = np.array([[0.1, 0.9],
+               [0.1, 0.9]])
+std = .5
+mean = 0
 
 # lambda_bar = get_lambda_bar(Pi)
 e_jt = np.array([0.8, 1.2])
@@ -131,15 +184,13 @@ e_jt = np.array([0.8, 1.2])
 # lambda_bar = get_lambda_bar(Pi)
 # e_jt = np.array([0.6, 0.8, 1.0, 1.3, 1.6, 2.0, 2.5])
 
-household_params = (N, S, J, beta_annual, sigma, Pi, e_jt)
+household_params = (N, S, J, beta_annual, sigma, Pi, e_jt, mean, std)
 
 # Firm parameters
 A = 1.0
 alpha = .35
 delta_annual = .05
-
 firm_params = (A, alpha, delta_annual)
-
 
 # SS parameters
 b_guess = np.ones((S, J))*.0001
@@ -149,7 +200,5 @@ rho = .5
 
 #calculation
 og = OG(household_params, firm_params)
-
-
 
 
